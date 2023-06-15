4f2dcb51d5fcd481c7a26b72e5638f1875c3cfc8
cyprian wanyama <wanyamacyprian2@gmail.com>
2023-06-15 14:35:26 -0700

    code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, sizeof(message) - 1);
-	return (1);
+    const char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, m, sizeof(m) - 1);
+    return (1);
 }
