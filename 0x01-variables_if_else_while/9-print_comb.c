1a5997f6453b7343af5da84b500173a18207b521
cyprian wanyama <wanyamacyprian2@gmail.com>
2023-06-18 04:49:17 -0700

    program to output nums separated by commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,16 +2,16 @@
 /**
  * main-outputs nums separated by commas
  *
- * return (0)
+ * return: (0)
  */
 int main(void)
 {
 	int num='0';
 
-	while(num<='9')
+	while(num <= '9')
 	{
 		putchar(num);
-		if (num <='8')
+		if (num <= '8')
 		{
 			putchar(',');
 			putchar(' ');
