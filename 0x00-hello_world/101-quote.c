5967f6dd9f652a1d1b0ded1698dae1b3626956a9
cyprian wanyama <wanyamacyprian2@gmail.com>
2023-06-19 12:45:50 -0700

    C program that prints a string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	const char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, m, sizeof(m) - 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
