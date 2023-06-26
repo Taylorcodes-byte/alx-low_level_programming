e450e293e9b87b6b55c80d65cd2374772c5f049c
cyprian wanyama <wanyamacyprian2@gmail.com>
2023-06-26 12:05:54 -0700

    fgashfj

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+i#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num1 = 1;
+	int num1 = 2;
+
+	printf("Before swap: num1 = %d, num2 = %d\n", num1, num2);
+
+	swap_int(&num1, &num2);
+	printf("After swap: num1 = %d, num2 = %d\n", num1, num2);
+	return (0);
+}
